607bc6b918c62c70bb10d4bcd57a125fa75a6128
Michael Fernandes <>
2023-04-06 12:55:11 +0000

    organizando os códigos e o replit

--- EstruturaSequencial/exercicio16.py
+++ EstruturaSequencial/exercicio16.py
@@ -1,25 +1,28 @@
-print('Faça um programa para uma loja de tintas. O programa deverá pedir o tamanho em metros quadrados da área a ser pintada. Considere que a cobertura da tinta é de 1 litro para cada 3 metros quadrados e que a tinta é vendida em latas de 18 litros, que custam R$ 80,00. Informe ao usuário a quantidades de latas de tinta a serem compradas e o preço total.')
-areapintada = float (input('Quantos metros quadrados serão pintados?'))
-#litrotinta = 18
-cobertura = areapintada / 3
-precotinta = 80
-precototal = 
-quantidadetinta = 0
-if areapintada>= 18:
-  quantidadetinta = 
+try:
+  tamanho  =  float (input('Entre com o tamanho da área: ' ))
 
-
-
-
-
-tamanho = float(input('Tamanho em metros quadrados: '))
-litros = tamanho / 3
-
-if tamanho % 54 == 0:
-	latas = tamanho / 54
-else: 
-	latas = int(tamanho / 54) + 1
-
-preco = latas * 80
-print ('%d latas' %latas)
-print ('R$ %.2f' %preco)
+  litros  =  tamanho  /  6
+  latas  =  litros  /  18
+  
+  if  latas % 18 !=  0 :
+    latas  +=  1
+  preço  =  latas  *  80
+  
+  galões  =  litros  /  3.6
+  if galões  %  3.6  !=  0 :
+    galões  +=  1
+    preco2  =  galões  *  25
+  
+  # mistura de latas e galões
+  mistura_lata  =  int ( litros  /  18.0 )
+  mistura_galao  =  int (( litros  - ( mistura_lata  *  18 )) /  3.6 )
+  
+  if  litros  - ( mistura_lata  *  18 ) %  3.6  !=  0 :
+      mistura_galao  +=  1
+  
+  print ( 'Apenas latas de 18 litros: %d'  %  latas , 'preço: %d'  %  preço )
+  print ( 'Apenas galões de 3.6 litros: %d'  %  galoes , 'preço: %d'  %  preco2 )
+  print ( 'Mistura: %d latas e %d galões = %.2f'  % (mistura_lata , mistura_galao , (( mistura_lata  *  80 ) +        
+ (mistura_galao  *  25 ))))
+except:
+  print('dados incorretos  ')
